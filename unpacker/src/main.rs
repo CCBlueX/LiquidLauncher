bbffc3a6414527e16f1d1d0d8559307d5fa89e35
1zuna <marco@ccbluex.net>
2022-10-05 20:47:12 +0200

    added basic unpacker logic

--- unpacker/src/main.rs
+++ unpacker/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::path::Path;
+use rust_embed::RustEmbed;
+use anyhow::Result;
+
+#[derive(RustEmbed)]
+#[folder = "data/"]
+struct Data;
+
+fn main() -> Result<()> {
+    for file_name in Data::iter() {
+        println!("extracting {}", file_name.as_ref());
+
+        if let Some(file) = Data::get(file_name.as_ref()) {
+            let path = Path::new(file_name.as_ref());
+            if let Some(parent) = path.parent() {
+                fs::create_dir_all(parent)?;
+            }
+            fs::write(path, file.data)?;
+        }
+    }
+    Ok(())
 }
