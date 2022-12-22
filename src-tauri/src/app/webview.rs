bf95b115adf04622e9151e262ccebb5f623eb95b
1zuna <marco@ccbluex.net>
2022-12-22 01:18:57 +0100

    added working webview

--- src-tauri/src/app/webview.rs
+++ src-tauri/src/app/webview.rs
@@ -2,57 +2,52 @@
 //use web_view::Content;
 // use std::sync::{Arc, Mutex};
 
+use std::sync::{Arc, Mutex};
+
 use anyhow::Result;
+use log::info;
+use tauri::Manager;
 
 // use log::info;
-use crate::utils::download_file;
+use crate::{utils::download_file, error::LauncherError};
 
-pub(crate) async fn download_client<F>(url: &str, on_progress: F) -> Result<Vec<u8>> where F : Fn(u64, u64) {
-//     info!("Retrieving LiquidBounce download url from {}", url);
+pub(crate) async fn download_client<F>(url: &str, on_progress: F, window: &Arc<Mutex<tauri::Window>>) -> Result<Vec<u8>> where F : Fn(u64, u64) {
+    let app_handle = window.lock().unwrap().app_handle();
 
-//     let download_link_cell = Arc::new(Mutex::new(None));
+    let download_page = format!("{}&liquidlauncher=1", url);
+    let download_view = tauri::WindowBuilder::new(
+        &app_handle,
+        "download",
+        tauri::WindowUrl::External(download_page.parse().unwrap())
+    )
+        .title("Download of LiquidBounce")
+        .user_agent("LiquidLauncher")
+        .center()
+        .focused(true)
+        .maximized(true)
+        .build().unwrap();
+    
+    // Show and maximize the download view
+    download_view.show().unwrap();
+    download_view.maximize().unwrap();
 
-//     let cloned_cell = download_link_cell.clone();
+    // Wait for the download to finish
+    let download_link_cell = Arc::new(Mutex::new(None));
+    let cloned_cell = download_link_cell.clone();
 
-//     {
-//         let mut webview = web_view::builder()
-//             .title("Download LiquidBounce")
-//             .content(Content::Url(format!("{}&liquidlauncher={}", url, 0)))
-//             .size(1000, 600)
-//             .resizable(true)
-//             .debug(true)
-//             .user_data(())
-//             .invoke_handler(move |webview, arg| {
-//                 let mut split = arg.split('|');
+    download_view.once("download", move |event| {
+        if let Some(payload) = event.payload() {
+            *cloned_cell.lock().unwrap() = Some(payload.to_owned());
+        }
+    });
 
-//                 if let Some(cmd) = split.next() {
-//                     if cmd == "download" {
-//                         if let Some(dl_url) = split.next() {
-//                             *cloned_cell.lock().unwrap() = Some(dl_url.to_owned());
-
-//                             webview.exit();
-
-//                             return Ok(());
-//                         }
-//                     }
-//                 }
-
-//                 Err(web_view::Error::Custom(Box::new(LauncherError::InvalidJavaScript("Invalid command".to_owned()))))
-//             })
-//             .build()?;
-
-//         webview.set_maximized(true);
-
-//         webview.run()?;
-//     }
-
-//     let url = {
-//         let mg= download_link_cell.lock().unwrap();
-
-//         mg.as_ref().ok_or_else(|| LauncherError::InvalidJavaScript("Failed to retrieve the download link".to_owned()))?.to_owned()
-//     };
-
-//     info!("Downloading LiquidBounce from {}", url);
+    let url = {
+        let mg= download_link_cell.lock().unwrap();
+        
+        mg.as_ref().ok_or_else(|| LauncherError::InvalidJavaScript("Failed to retrieve the download link".to_owned()))?.to_owned()
+    };
+        
+    info!("Downloading LiquidBounce from {}", url);
 
     return download_file(&url, on_progress).await;
 }
