1d39c115228829794501798f6acf5110c33379f3
1zuna <marco@ccbluex.net>
2023-01-30 23:06:10 +0100

    Added support for webview download
    required pull request https://github.com/tauri-apps/tauri/pull/5918

--- src-tauri/src/app/webview.rs
+++ src-tauri/src/app/webview.rs
@@ -2,22 +2,22 @@
 //use web_view::Content;
 // use std::sync::{Arc, Mutex};
 
-use std::sync::{Arc, Mutex};
-
+use std::{sync::{Arc, Mutex}, time::Duration};
+use serde::Deserialize;
 use anyhow::Result;
-use log::info;
+use log::{info, debug};
 use tauri::Manager;
 
 // use log::info;
 use crate::{utils::download_file, error::LauncherError};
 
-pub(crate) async fn download_client<F>(url: &str, on_progress: F, window: &Arc<Mutex<tauri::Window>>) -> Result<Vec<u8>> where F : Fn(u64, u64) {
+pub(crate) async fn download_client<F>(url: &str, on_progress: F, window: &Arc<Mutex<tauri::Window>>) -> Result<Vec<u8>> where F : Fn(u64, u64){
     let app_handle = window.lock().unwrap().app_handle();
 
     let download_page = format!("{}&liquidlauncher=1", url);
     let download_view = tauri::WindowBuilder::new(
         &app_handle,
-        "download",
+        "client_download",
         tauri::WindowUrl::External(download_page.parse().unwrap())
     )
         .title("Download of LiquidBounce")
@@ -36,18 +36,31 @@ pub(crate) async fn download_client<F>(url: &str, on_progress: F, window: &Arc<M
     let cloned_cell = download_link_cell.clone();
 
     download_view.once("download", move |event| {
+        debug!("Triggerd download event");
+
         if let Some(payload) = event.payload() {
-            *cloned_cell.lock().unwrap() = Some(payload.to_owned());
+            #[derive(Deserialize)]
+            struct DownloadPayload {
+                url: String
+            }
+
+            let payload = serde_json::from_str::<DownloadPayload>(payload).unwrap();
+
+            info!("Received download link: {}", payload.url);
+            *cloned_cell.lock().unwrap() = Some(payload.url);
         }
     });
 
-    let url = {
-        let mg= download_link_cell.lock().unwrap();
-        
-        mg.as_ref().ok_or_else(|| LauncherError::InvalidJavaScript("Failed to retrieve the download link".to_owned()))?.to_owned()
+    let url = loop {
+        if let Ok(mg) = download_link_cell.lock() {
+            if let Some(received) = mg.clone() {
+                break received;
+            }
+        }
     };
-        
-    info!("Downloading LiquidBounce from {}", url);
 
+    download_view.close().unwrap();
+
+    info!("Downloading LiquidBounce from {}", url);
     return download_file(&url, on_progress).await;
 }
