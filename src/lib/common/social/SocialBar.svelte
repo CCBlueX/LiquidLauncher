2ad34b889df31988072a1c792894fd350b80e5ca
1zuna <marco@ccbluex.net>
2023-12-07 20:59:58 +0100

    updated forum url

--- src/lib/common/social/SocialBar.svelte
+++ src/lib/common/social/SocialBar.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="social-bar">
-    <ButtonIcon icon="nodebb" text="Forum" url="https://forums.ccbluex.net/"/>
+    <ButtonIcon icon="nodebb" text="Forum" url="https://forum.liquidbounce.net/"/>
     <ButtonIcon icon="github" text="GitHub" url="https://github.com/CCBlueX/"/>
     <ButtonIcon icon="guilded" text="Guilded" url="https://guilded.gg/CCBlueX" />
     <ButtonIcon icon="twitter" text="Twitter" url="https://twitter.com/ccbluex"/>
