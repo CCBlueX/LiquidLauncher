2d5228cd714b38d4abf7f9fb8e86109080d91159
Izuna Seikatsu <izuna.seikatsu@ccbluex.net>
2026-09-23 21:11:22 +0200

    feat: give custom mods a version line and an action slot

--- src/lib/settings/CustomModSetting.svelte
+++ src/lib/settings/CustomModSetting.svelte
@@ -4,24 +4,50 @@
 
     export let value;
     export let title;
+    export let lined = false;
 
     const dispatch = createEventDispatcher();
 </script>
 
 <div class="custom-mod-setting">
-    <ToggleSetting bind:value={value} {title} disabled={false} on:change />
-    <button class="button-delete" on:click={() => dispatch("delete", { name: title })}>
-        <img src="img/icon/icon-button-close.svg" alt="delete" title="Remove mod">
-    </button>
+    <div>
+        <ToggleSetting bind:value={value} {title} disabled={false} on:change />
+        {#if lined}
+            <div class="line"><slot name="line" /></div>
+        {/if}
+    </div>
+    <div class="side">
+        <button class="button-delete" on:click={() => dispatch("delete", { name: title })}>
+            <img src="img/icon/icon-button-close.svg" alt="delete" title="Remove mod">
+        </button>
+        <slot name="side" />
+    </div>
 </div>
 
 <style>
     .custom-mod-setting {
         display: grid;
-        grid-template-columns: 1fr max-content;
+        grid-template-columns: minmax(0, 1fr) max-content;
         gap: 5px;
+        align-items: center;
     }
-    
+
+    .line {
+        padding-left: 30px;
+        font-size: 12px;
+        line-height: 15px;
+        color: rgba(255, 255, 255, .5);
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+
+    .side {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+    }
+
     .button-delete {
         background-color: transparent;
         display: flex;
@@ -37,7 +63,7 @@
     .custom-mod-setting:hover .button-delete {
         opacity: 1;
     }
-    
+
     .button-delete img {
         height: 10px;
     }
